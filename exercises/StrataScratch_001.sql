290aa54a958249e570092ca88a475f9453fcf4ed
Viv <121664534+vivisn00b@users.noreply.github.com>
2026-08-11 16:50:29 +0530

    Refactor SQL query to use INNER JOIN and DISTINCT

--- exercises/StrataScratch_001.sql
+++ exercises/StrataScratch_001.sql
@@ -1,8 +1,8 @@
 -- Find all posts which were reacted to with a heart
 -- Find all posts which were reacted to with a heart. For such posts output all columns from facebook_posts table.
 
-select * 
-from facebook_posts as fp
-left join facebook_reactions as fr
-on fp.post_id = fr.post_id
-where fr.reaction = 'heart';
+SELECT DISTINCT p.*
+FROM facebook_posts p
+INNER JOIN facebook_reactions r 
+ON p.post_id = r.post_id AND 
+   r.reaction = 'heart';
